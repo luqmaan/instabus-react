85c754b9aa0f30e495fa9291506c45192fa62e8e
Luqmaan <ldawoodjee@gmail.com>
2015-02-15 18:46:18 -0600

    Flesh out static app
    Add routes list
    Add arrivals list
    Move static data into props

--- client/js/app.js
+++ client/js/app.js
@@ -5,49 +5,45 @@ var {Map, Marker, Popup, TileLayer, Polyline, CircleMarker} = Leaflet;
 var position = [30.267153, -97.743061];
 
 
-var polylinesFetcher = window.polylinesFetcher = {
-    '801': [
-        require('../data/shapes_801_0.json'),
-        require('../data/shapes_801_1.json'),
-    ]
-};
-var stopsFetcher = window.stopsFetcher = {
-    '801': require('../data/stops_801_0.json').concat(require('../data/stops_801_1.json')),
-};
-var vehiclesFetcher = window.vehiclesFetcher = {
-    '801': require('../data/vehicles_801.json'),
-};
+var RoutesList = React.createClass({
+
+    render() {
+        var routes = this.props.routes.map((route) => {
+            return (
+                <li key={route.route_id}>
+                    <span>{route.route_id}</span>
+                    <span>{route.name}</span>
+                </li>
+            );
+        })
+
+        return (
+            <ul>
+                {routes}
+            </ul>
+        );
+    }
+
+});
 
 
 var MapController = React.createClass({
 
-    getPolylines() {
-        return polylinesFetcher[this.props.routeId];
-    },
-
-    getStops() {
-        return stopsFetcher[this.props.routeId];
-    },
-
-    getVehicles() {
-        return vehiclesFetcher[this.props.routeId].NextBus2.query.results.Envelope.Body.Nextbus2Response.Runs.Run;
-    },
-
     render() {
-        var polylines = this.getPolylines().map((positions, index) => {
+        var polylinesList = this.props.polylines.map((positions, index) => {
             return (
                 <Polyline
                     positions={positions}
-                    key={this.props.routeId + "_" + index}
+                    key={'polyline:' + this.props.routeId + '_' + index}
                 />
             );
         }, this);
 
-        var stops = this.getStops().map((stop, index) => {
+        var stopsList = this.props.stops.map((stop, index) => {
             return (
                 <CircleMarker
                     center={{lat: stop.stop_lat, lng: stop.stop_lon}}
-                    key={this.props.routeId + "_" + stop.direction_id + "_" + stop.stop_id}
+                    key={'stop:' + this.props.routeId + '_' + stop.direction_id + '_' + stop.stop_id}
                 >
                     <Popup>
                         <span>{stop.stop_name}</span>
@@ -56,11 +52,14 @@ var MapController = React.createClass({
             );
         }, this);
 
-        var vehicles = this.getVehicles().map((vehicle, index) => {
+
+        var vehiclesList = this.props.vehicles.map((vehicle, index) => {
+            var center = {lat: Number(vehicle.Realtime.Lat), lng: Number(vehicle.Realtime.Long)};
+
             return (
                 <CircleMarker
-                    center={{lat: vehicle.Realtime.Lat, lng: vehicle.Realtime.Long}}
-                    key={vehicle.Vehicleid}
+                    center={center}
+                    key={'vehicle:' + vehicle.Vehicleid + index}
                     className='vehicle-marker'
                 >
                     <Popup>
@@ -72,24 +71,56 @@ var MapController = React.createClass({
         });
 
         return (
-            <Map center={position} zoom={Number(this.props.zoom)} id='map'>
+            <Map center={position} zoom={13} id='map'>
                 <TileLayer
                     url='https://{s}.tiles.mapbox.com/v3/{id}/{z}/{x}/{y}.png'
                     attribution='<a href="http://openstreetmap.org">OpenStreetMap</a> | <a href="http://mapbox.com">Mapbox</a>'
                     id='drmaples.ipbindf8'
                 />
-                {polylines}
-                {stops}
-                {vehicles}
+                {polylinesList}
+                {stopsList}
+                {vehiclesList}
             </Map>
         );
     },
 
 });
 
-React.render(<MapController routeId="801" zoom="13" />, document.getElementById('map-container'));
+var Arrivals = React.createClass({
+
+    render() {
+        var arrivals = this.props.arrivals.map((arrival) => {
+            return (
+                <li key={'arrival:' + arrival.vId}>{arrival.est}</li>
+            );
+        });
+        return (
+            <ul>
+                {arrivals}
+            </ul>
+        );
+    }
+});
+
+var staticRoutes = require('../data/routes.json');
+var staticPolylines = [require('../data/shapes_801_0.json'), require('../data/shapes_801_1.json')];
+var staticStops = require('../data/stops_801_0.json').concat(require('../data/stops_801_1.json'));
+var staticVehicles = require('../data/vehicles_801.json').NextBus2.query.results.Envelope.Body.Nextbus2Response.Runs.Run;
+var staticArrivals = require('../data/arrivals.json').list;
+
+var App = React.createClass({
+    render() {
+        return (
+            <div>
+                <RoutesList routes={staticRoutes} />
+                <Arrivals arrivals={staticArrivals} />
+                <MapController routeId='801' polylines={staticPolylines} vehicles={staticVehicles} stops={staticStops} />
+            </div>
+        );
+    }
+});
+
+React.render(<App />, document.getElementById('app'));
 
 
-window.MapController = MapController
-
-// module.exports = window.App = App;
+module.exports = window.App = App;
