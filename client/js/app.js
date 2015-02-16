bb13addacf80867d358ce47a29cf08a67ed93e14
Luqmaan <ldawoodjee@gmail.com>
2015-02-15 23:18:03 -0600

    App.activeRoute as a state, and update the map with it

--- client/js/app.js
+++ client/js/app.js
@@ -10,12 +10,12 @@ var RoutesList = React.createClass({
     render() {
         var routes = this.props.routes.map((route) => {
             return (
-                <li key={route.route_id}>
+                <li key={route.route_id} onClick={this.props.onRouteSelected.bind(null, route.route_id)}>
                     <span>{route.route_id}</span>
                     <span>{route.name}</span>
                 </li>
             );
-        })
+        }, this);
 
         return (
             <ul>
@@ -34,8 +34,7 @@ var MapController = React.createClass({
             return (
                 <Polyline
                     positions={positions}
-                    key={'polyline:' + this.props.routeId + '_' + index}
-                />
+                    key={'polyline:' + this.props.routeId + ':' + index} />
             );
         }, this);
 
@@ -43,8 +42,7 @@ var MapController = React.createClass({
             return (
                 <CircleMarker
                     center={{lat: stop.stop_lat, lng: stop.stop_lon}}
-                    key={'stop:' + this.props.routeId + '_' + stop.direction_id + '_' + stop.stop_id}
-                >
+                    key={'stop:' + this.props.routeId + ':' + stop.direction_id + ':' + stop.stop_id}>
                     <Popup>
                         <span>{stop.stop_name}</span>
                     </Popup>
@@ -54,17 +52,13 @@ var MapController = React.createClass({
 
 
         var vehiclesList = this.props.vehicles.map((vehicle, index) => {
-            var center = {lat: Number(vehicle.Realtime.Lat), lng: Number(vehicle.Realtime.Long)};
-
             return (
                 <CircleMarker
-                    center={center}
-                    key={'vehicle:' + vehicle.Vehicleid + index}
-                    className='vehicle-marker'
-                >
+                    center={vehicle}
+                    key={'vehicle:' + vehicle.vId + ':' + index}
+                    className='vehicle-marker'>
                     <Popup>
-                        <div>{vehicle.Vehicleid}</div>
-                        <div>{vehicle.Realtime.Polltime}</div>
+                        <span>{vehicle.Routeid}</span>
                     </Popup>
                 </CircleMarker>
             );
@@ -75,8 +69,7 @@ var MapController = React.createClass({
                 <TileLayer
                     url='https://{s}.tiles.mapbox.com/v3/{id}/{z}/{x}/{y}.png'
                     attribution='<a href="http://openstreetmap.org">OpenStreetMap</a> | <a href="http://mapbox.com">Mapbox</a>'
-                    id='drmaples.ipbindf8'
-                />
+                    id='drmaples.ipbindf8' />
                 {polylinesList}
                 {stopsList}
                 {vehiclesList}
@@ -103,18 +96,52 @@ var Arrivals = React.createClass({
 });
 
 var staticRoutes = require('../data/routes.json');
-var staticPolylines = [require('../data/shapes_801_0.json'), require('../data/shapes_801_1.json')];
-var staticStops = require('../data/stops_801_0.json').concat(require('../data/stops_801_1.json'));
-var staticVehicles = require('../data/vehicles_801.json').NextBus2.query.results.Envelope.Body.Nextbus2Response.Runs.Run;
-var staticArrivals = require('../data/arrivals.json').list;
+var staticPolylines = {
+    801: [require('../data/shapes_801_0.json'), require('../data/shapes_801_1.json')],
+    550: [require('../data/shapes_550_0.json'), require('../data/shapes_550_1.json')],
+    803: [require('../data/shapes_803_0.json'), require('../data/shapes_803_1.json')],
+};
+var staticStops = {
+    801: require('../data/stops_801_0.json').concat(require('../data/stops_801_1.json')),
+    550: require('../data/stops_550_0.json').concat(require('../data/stops_550_1.json')),
+    803: require('../data/stops_803_0.json').concat(require('../data/stops_803_1.json')),
+}
+// var staticVehicles = require('../data/vehicles_801.json').NextBus2.query.results.Envelope.Body.Nextbus2Response.Runs.Run;
+var staticArrivals = require('../data/arrivals.json');
 
 var App = React.createClass({
+
+    getInitialState() {
+        return {
+            activeRouteId: null,
+        };
+    },
+
+    handleRouteChange(routeId) {
+        console.log(routeId)
+        this.setState({
+            activeRouteId: routeId
+        });
+    },
+
     render() {
+        var routeDetails = !this.state.activeRouteId ? null : (
+            <div>
+                <h1>{this.state.activeRouteId}</h1>
+                <Arrivals arrivals={staticArrivals[this.state.activeRouteId]} />
+                <MapController
+                    id="map-controller"
+                    routeId={this.state.activeRouteId}
+                    polylines={staticPolylines[this.state.activeRouteId]}
+                    vehicles={staticArrivals[this.state.activeRouteId]}
+                    stops={staticStops[this.state.activeRouteId]} />
+            </div>
+        );
+
         return (
             <div>
-                <RoutesList routes={staticRoutes} />
-                <Arrivals arrivals={staticArrivals} />
-                <MapController routeId='801' polylines={staticPolylines} vehicles={staticVehicles} stops={staticStops} />
+                <RoutesList routes={staticRoutes} activeRouteId={this.state.activeRouteId} onRouteSelected={this.handleRouteChange} />
+                {routeDetails}
             </div>
         );
     }
